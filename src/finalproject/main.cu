6c927e4fcd5abec69754c3326d919fb2ded4b59d
Yuri Gorokhov <yurigorokhov@gmail.com>
2012-05-02 00:20:52 -0700

    14848x14848

--- src/finalproject/main.cu
+++ src/finalproject/main.cu
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <sys/time.h>
 
-#define MAT_SIZE 10240
+#define MAT_SIZE 512*29
 #define BLOCK_SIZE 512
 #define MAX_ELEMENT 12
 
@@ -44,12 +44,12 @@ int main(int argc, char**argv) {
   cudaEventCreate(&stop);
   
   // Copy matrix to global memory
-  int *DevMat, *dev_shortest_path, *dev_result_stack, *dev_calc_matrix;
+  int *DevMat, *dev_shortest_path, *dev_result_stack;
   cudasafe( cudaMalloc((void**)&dev_shortest_path, sizeof(int)), "cudaMalloc" );
   cudasafe( cudaMalloc((void**)&DevMat, MAT_SIZE * MAT_SIZE * sizeof(int)), "cudaMalloc" );
-  cudasafe( cudaMalloc((void**)&dev_result_stack, MAT_SIZE * 2 * sizeof(int)), "cudaMalloc" );
+  if(argc > 1)
+    cudasafe( cudaMalloc((void**)&dev_result_stack, MAT_SIZE * 2 * sizeof(int)), "cudaMalloc" );
   cudasafe( cudaMemcpy(DevMat, mat.array, MAT_SIZE * MAT_SIZE * sizeof(int), cudaMemcpyHostToDevice), "cudaMemcpy" );
-  cudasafe( cudaMalloc((void**)&dev_calc_matrix, MAT_SIZE * MAT_SIZE * sizeof(int)), "cudaMalloc" );
   
   // Compute shortest path with cpu
   int shortestpath = 0;
@@ -58,23 +58,23 @@ int main(int argc, char**argv) {
   dim3 threadsPerBlock(BLOCK_SIZE);
   for(int i = 1; i < MAT_SIZE; i++) {
     dim3 blocks((int)ceil((float)(i+1) / (float)threadsPerBlock.x));
-    shortest_path_cuda<<<blocks,threadsPerBlock>>>(DevMat, dev_shortest_path, dev_result_stack, dev_calc_matrix, i);
+    shortest_path_cuda<<<blocks,threadsPerBlock>>>(DevMat, dev_shortest_path, dev_result_stack, i);
   }
   for(int i = 1; i < MAT_SIZE; i++) {    
     dim3 blocks((int)ceil((float)(MAT_SIZE-i) / (float)threadsPerBlock.x));
-    shortest_path_cuda_2<<<blocks,threadsPerBlock>>>(DevMat, dev_shortest_path, dev_result_stack, dev_calc_matrix, i);
+    shortest_path_cuda_2<<<blocks,threadsPerBlock>>>(DevMat, dev_shortest_path, dev_result_stack, i);
   }
   if(argc > 1)
-    shortest_path_cuda_3<<<1,1>>>(DevMat, dev_shortest_path, dev_result_stack, dev_calc_matrix);
+    shortest_path_cuda_3<<<1,1>>>(DevMat, dev_shortest_path, dev_result_stack);
   cudaEventRecord(stop, 0);
   cudaEventSynchronize(stop);
   cudaEventElapsedTime(&elapsedTime, start, stop);
   cudasafe( cudaMemcpy(&shortestpath, dev_shortest_path, sizeof(int), cudaMemcpyDeviceToHost) ,"cudaMemcpy");
   cudasafe( cudaMemcpy(result_stack, dev_result_stack, MAT_SIZE * 2 * sizeof(int), cudaMemcpyDeviceToHost) ,"cudaMemcpy");
   cudasafe( cudaFree(DevMat), "cudaFree" );
-  cudasafe( cudaFree(dev_result_stack), "cudaFree" );
+  if(argc > 1)
+    cudasafe( cudaFree(dev_result_stack), "cudaFree" );
   cudasafe( cudaFree(dev_shortest_path), "cudaFree" );
-  cudasafe( cudaFree(dev_calc_matrix), "cudaFree" );
   
   // Print path taken 
   printf("\nelapsed time: %f\n", elapsedTime);
--- src/finalproject/shortestpath_cuda.h
+++ src/finalproject/shortestpath_cuda.h
@@ -7,31 +7,27 @@
  * Path is returned as 1 for right, 0 for down
  * Note: this algorithm goes from top left to bottom right corner
  */
-__global__ void shortest_path_cuda(int * mat, int * shortestpath, int * result, int * matrix, int i) {
-  
+__global__ void shortest_path_cuda(int * mat, int * shortestpath, int * result, int i) {
   int x = blockIdx.x * BLOCK_SIZE + threadIdx.x;
-  if(x == 0 && i == 1) {
-    matrix[0] = mat[0];
-  }
     
     // only use the threads we need
     if(x <= i) {
       int idx = x * MAT_SIZE + i - x;
       if(x == 0) {
-	matrix[idx] = matrix[idx-1] + mat[idx];
+	mat[idx] = mat[idx-1] + mat[idx];
       } else if(i - x == 0) {
-	matrix[idx] = matrix[idx - MAT_SIZE] + mat[idx];
+	mat[idx] = mat[idx - MAT_SIZE] + mat[idx];
       } else {	
 	int path1 = idx - 1;
 	int path2 = idx - MAT_SIZE;
-	matrix[idx] = matrix[path1] < matrix[path2] 
-		      ? matrix[path1] + mat[idx] 
-		      : matrix[path2] + mat[idx];
+	mat[idx] = mat[path1] < mat[path2] 
+		      ? mat[path1] + mat[idx] 
+		      : mat[path2] + mat[idx];
       }
     }
 }
 
-__global__ void shortest_path_cuda_2(int * mat, int * shortestpath, int * result, int * matrix, int i) { 
+__global__ void shortest_path_cuda_2(int * mat, int * shortestpath, int * result, int i) { 
     int x = blockIdx.x * BLOCK_SIZE + threadIdx.x;
     
     // only use the threads we need
@@ -39,18 +35,17 @@ __global__ void shortest_path_cuda_2(int * mat, int * shortestpath, int * result
       int idx = (MAT_SIZE - x - 1) + (x + i) * MAT_SIZE;
       int path1 = idx - 1;
       int path2 = idx - MAT_SIZE;
-      matrix[idx] = matrix[path1] < matrix[path2] 
-		    ? matrix[path1] + mat[idx] 
-		    : matrix[path2] + mat[idx];
+      mat[idx] = mat[path1] < mat[path2] 
+		    ? mat[path1] + mat[idx] 
+		    : mat[path2] + mat[idx];
     }
     if(threadIdx.x == 0 && i == MAT_SIZE-1) {
-      *shortestpath = matrix[MAT_SIZE * MAT_SIZE - 1];
+      *shortestpath = mat[MAT_SIZE * MAT_SIZE - 1];
     }
 }
 
-__global__ void shortest_path_cuda_3(int * mat, int * shortestpath, int * result, int * matrix) {
+__global__ void shortest_path_cuda_3(int * mat, int * shortestpath, int * result) {
   if(threadIdx.x == 0) {
-    //*shortestpath = matrix[MAT_SIZE * MAT_SIZE - 1];
 
     // Put shortest path onto stack
     int j = MAT_SIZE-1, k = MAT_SIZE-1;
@@ -63,8 +58,8 @@ __global__ void shortest_path_cuda_3(int * mat, int * shortestpath, int * result
 	j--;
 	result[index++] = 1;
       } else {
-	int path1 = matrix[MAT_SIZE * (k-1) + j];
-	int path2 = matrix[MAT_SIZE * k + j-1];
+	int path1 = mat[MAT_SIZE * (k-1) + j];
+	int path2 = mat[MAT_SIZE * k + j-1];
 	if(path1 < path2) {
 	  k--;
 	  result[index++] = 0;
